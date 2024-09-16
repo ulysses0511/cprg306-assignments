9a2bb8d39061df3c1acf419c37dbbdb249e1c48d
Tsz Kin Fan <151567419+ulysses0511@users.noreply.github.com>
2024-09-16 10:44:16 -0700

    Completed Week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <p>Tsz Kin Fan</p>
+            <p>
+                <Link href="https://github.com/ulysses0511/cprg306-assignments.git">https://github.com/ulysses0511/cprg306-assignments.git</Link>
+            </p>
+        </div>
+    );
+}
