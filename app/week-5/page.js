9d972c7e6c5db1383f7a81b54981248e0aecfaf1
Tsz Kin Fan <151567419+ulysses0511@users.noreply.github.com>
2024-10-13 20:35:25 -0700

    Completed Week 5 assignment

---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-item";
+
+export default function Page(){
+    return (
+        <main className="min-h-screen bg-black flex items-center justify-center">
+            <NewItem/>
+        </main>
+    );
+}
